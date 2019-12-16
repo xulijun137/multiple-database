98b9287b059f61072f53e627d16e871a61f7268a
xulijun <xulijun137@163.com>
2019-12-16 16:51:49 +0800

    提交修改

--- src/main/java/com/example/bean/DBContextHolder.java
+++ src/main/java/com/example/bean/DBContextHolder.java
@@ -9,10 +9,11 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class DBContextHolder {
 
     private static final ThreadLocal<DBTypeEnum> contextHolder = new ThreadLocal<>();
-    //计时器，每次自动增加1次
+
     private static final AtomicInteger counter = new AtomicInteger(-1);
 
     public static void set(DBTypeEnum dbType) {
+
         contextHolder.set(dbType);
     }
 
@@ -22,21 +23,21 @@ public class DBContextHolder {
 
     public static void master() {
         set(DBTypeEnum.MASTER);
-        log.info("------------------ 切换到master -----------------");
+        log.info("===================== 切换到master ========================");
     }
 
     public static void slave() {
-        //轮询
+        //  轮询
         int index = counter.getAndIncrement() % 2;
         if (counter.get() > 9999) {
             counter.set(-1);
         }
         if (index == 0) {
             set(DBTypeEnum.SLAVE1);
-            log.info("------------------ 切换到slave1 -----------------");
+            log.info("===================== 切换到slave1 ========================");
         }else {
             set(DBTypeEnum.SLAVE2);
-            log.info("------------------ 切换到slave2 -----------------");
+            log.info("===================== 切换到slave2 ========================");
         }
     }
 
--- src/main/java/com/example/bean/MyRoutingDataSource.java
+++ src/main/java/com/example/bean/MyRoutingDataSource.java
@@ -1,6 +1,5 @@
 package com.example.bean;
 
-import com.example.utils.DBContextHolder;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 import org.springframework.lang.Nullable;
 
